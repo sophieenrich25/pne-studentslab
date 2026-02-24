a3219c3eb9b162b28ac30c4298f8b4b8380fc302
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-02-24 16:42:04 +0100

    SO6-EX1

---
+++ S06/seq-01.py
@@ -0,0 +1,35 @@
+class Seq:
+    """A class for representing sequences"""
+    pass
+
+s1 = Seq()
+s2 = Seq()
+
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self):
+        print("New sequence created!") # It's not good practice to print here but let's make an exception!
+
+
+# Main program
+# Create an object of the class Seq
+s1 = Seq()
+s2 = Seq()
+print("Testing...")
+
+
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        self.strbases = strbases
+        print("New sequence created!")
+
+
+# Main program
+# Create objects of the class Seq
+s1 = Seq("AGTACACTGGT")
+s2 = Seq("CGTAAC")
+
+
