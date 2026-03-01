521fbf2ac27a1da1e2b28558bac66c58078f9bf1
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-03-01 21:06:49 +0100

    SO6-EX3 and EX4

---
+++ S06/ex4.py
@@ -0,0 +1,34 @@
+from termcolor import colored
+class Seq:
+    def __init__(self, strbases):
+        self.strbases =strbases
+        print("New sequence created!")
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+
+def print_seqs(seq_list, color):
+    i = 0
+    for seq in seq_list:
+        print(colored((f"Sequence {i}: (Length: {seq.len()}) {seq}"), color))
+        i += 1
+
+def generate_seqs(pattern, number):
+    seq_list = []
+    for i in range(1, number + 1):
+        new_pattern = pattern * i
+        seq_list.append(Seq(new_pattern))
+    return seq_list
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1, "blue")
+
+print()
+print("List 2:")
+print_seqs(seq_list2, "green")
