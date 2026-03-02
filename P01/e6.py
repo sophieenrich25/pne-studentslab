1aa97eb20b0414924fc75e95928e596bd7bb7cf3
ophieenrich25 <saenrich@gmail.com>
2026-03-02 22:11:10 +0100

    P01-ex6

---
+++ P01/e6.py
@@ -0,0 +1,13 @@
+from Seq1 import Seq, print_seqs
+
+print("----|Practice 1, exercise 6|----")
+s1 = Seq()
+s2 = Seq("ACTG")
+s3 = Seq("Invalid sequence")
+
+
+seqs = [s1, s2, s3]
+for i in range(len(seqs)):
+    seq = seqs[i]
+    print(f"Sequence {i}, : (Length: {seq.len()} ), {seq}")
+    print(seq.count())
