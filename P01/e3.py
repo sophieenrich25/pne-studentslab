a3cb687b5cd53e5ba7e3598419a90e456f3f900a
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-03-02 09:29:39 +0100

    P01-e3

---
+++ P01/e3.py
@@ -0,0 +1,9 @@
+from Seq1 import Seq
+
+print("----|Practice 1, exercise 3|----")
+s1 = Seq()
+s2 = Seq("TATAC")
+s3 = Seq("Invalid sequence")
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
+print(f"Sequence 3: {s3}")
