3194e2b025815f7382d585323d6a7923f46b4534
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-03-03 16:28:32 +0100

    s07

--- S08/e3.py
+++ S08/e3.py
@@ -4,7 +4,8 @@ PORT = 8081
 IP = "212.128.255.64"
 
 while True:
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((IP, PORT))
-  s.send(str.encode("HELLO FROM THE CLIENT!!!"))
-  s.close()
+    message = input("Enter a message:")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+    s.send(str.encode(message))
+    s.close()
--- S08/server.py
+++ S08/server.py
@@ -3,9 +3,10 @@ import socket
 PORT = 8081
 IP = "212.128.255.81"
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((IP, PORT))
-s.send(str.encode("HELLO FROM THE CLIENT!!!"))
-s.close()
+while True:
+    message = input("Enter a message:")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+    s.send(str.encode(message))
+    s.close()
 
