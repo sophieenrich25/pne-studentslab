f75c0255231ac2768bcc24dce9ce20efdc990acd
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-03-03 08:40:35 +0100

    EX5

--- P01/e5.py
+++ P01/e5.py
@@ -2,6 +2,15 @@ from Seq1 import Seq, print_seqs
 
 print("----|Practice 1, exercise 5|----")
 s1 = Seq()
-s2 = Seq("TATAC")
+s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
-print()
+
+
+seqs = [s1, s2, s3]
+for i in range(len(seqs)):
+    seq = seqs[i]
+    print(f"Sequence {i}, : (Length: {seq.len()} ) {seq}")
+    print("  A:", seq.count_base("A"),
+          "C:", seq.count_base("C"),
+          "T:", seq.count_base("T"),
+          "G:", seq.count_base("G"))
