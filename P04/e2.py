72f454e1d3ac52f617796c3c15d465a829689ca6
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-03-21 14:27:10 +0100

    P04

---
+++ P04/e2.py
@@ -0,0 +1,51 @@
+import socket
+import termcolor
+
+IP = "127.0.0.1"
+PORT = 8080
+
+def process_client(s):
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+    print("Message FROM CLIENT: ")
+
+    lines = req.split('\n')
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    if req_line.startswith("GET /info/A"):
+        try:
+            with open("html/info/A.html", "r") as f:
+                body = f.read()
+        except:
+            body = "<h1>Error: file not found</h1>"
+    else:
+        body = " "
+    status_line = "HTTP/1.1 200 OK\n"
+
+    header = "Content-Type: text/html\n"
+    header += f"Content-Length: {len(body)}\n"
+
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ls.bind((IP, PORT))
+ls.listen()
+
+print("Server running at http://127.0.0.1:8080")
+
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+        ls.close()
+        exit()
+    else:
+        process_client(cs)
+        cs.close()
