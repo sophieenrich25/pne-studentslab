5f92cb39cb5a80c2e9182ca38d985969e01c1174
sophie <sa.enrich.2025@alumnos.urjc.es>
2026-05-04 21:34:24 +0200

    final project

--- Final-project/server.py
+++ Final-project/server.py
@@ -8,121 +8,143 @@ import json
 
 PORT = 8080
 class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def get_ensembl_json(self, endpoint):
+        PARAMS = "?content-type=application/json"
+        RESOURCE = endpoint + PARAMS
+        conn = http.client.HTTPSConnection("rest.ensembl.org")
+        try:
+            conn.request("GET", RESOURCE)
+            r = conn.getresponse()
+            if r.status == 200:
+                response = r.read().decode("utf-8")
+                data = json.loads(response)
+                return data
+        except ConnectionRefusedError:
+            return "ERROR! Cannot connect to the Server"
+        return None
+
+    def send_html_response(self, contents, status=200):
+        self.send_response(status)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+        self.end_headers()
+        self.wfile.write(contents.encode())
+
+    def html_lists(self, items):
+        html_str = ""
+        for i in items:
+            html_str += f"<li>{i}</li>"
+        return html_str
+
     def do_GET(self):
         termcolor.cprint(self.requestline, 'green')
-
         url_path = urlparse(self.path)
         path = url_path.path
         arguments = parse_qs(url_path.query)
 
-        SERVER = "rest.ensembl.org"
-        PARAMS = "?content-type=application/json"
-
-
         if path == "/":
             contents = Path('main_page.html').read_text()
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(contents.encode()))
-            self.end_headers()
-            self.wfile.write(contents.encode())
-
+            self.send_html_response(contents)
         elif path == "/listSpecies":
-            conn = http.client.HTTPSConnection(SERVER)
-            endpoint = "/info/species"
-            RESOURCE = endpoint + PARAMS
-            try:
-                conn.request("GET", RESOURCE)
-                r1 = conn.getresponse()
-                print(f"Response received!: {r1.status} {r1.reason}\n")
-            except ConnectionRefusedError:
-                print("ERROR! Cannot connect to the Server")
-                exit()
-
-            if r1.status == 200:
-                response = r1.read().decode("utf-8")
-                data = json.loads(response)
-                species = data["species"]
-
-                limit = arguments.get("limit", [None])[0]
-                if limit and limit.isdigit():
-                    species = species[:int(limit)]
-                else:
-                    limit = len(species)
-
-                species_list = data["species"][:int(limit)]
-                names_list = []
-                for i in species_list:
-                    name = i["display_name"]
-                    names_list.append(name)
-                for name in names_list:
-                    names_list += f"<li>{name}</li>"
-
-                contents = Path('listSpecies.html').read_text()
-                result = contents.format(
-                    total_species=len(data["species"]),
-                    limit_value=limit,
-                    species_list=names_list
-                )
-                self.send_response(200)
-                self.send_header('Content-Type', 'text/html')
-                self.send_header('Content-Length', len(result.encode()))
-                self.end_headers()
-                self.wfile.write(result.encode())
-
+            self.listSpecies(arguments)
         elif path == "/karyotype":
-            conn = http.client.HTTPSConnection(SERVER)
-            endpoint = "/info/assembly
-            RESOURCE = endpoint + PARAMS
-            try:
-                conn.request("GET", RESOURCE)
-                r1 = conn.getresponse()
-                print(f"Response received!: {r1.status} {r1.reason}\n")
-            except ConnectionRefusedError:
-                print("ERROR! Cannot connect to the Server")
-                exit()
+            self.karyotype(arguments)
+        elif path == "/chromosomeLength":
+            self.chromosomeLength(arguments)
+        else:
+            self.error()
 
-            if r1.status == 200:
-                response = r1.read().decode("utf-8")
-                data = json.loads(response)
-                species = data["species"]
+    def error(self):
+        contents = Path('error.html').read_text()
+        print("Error")
+        return self.send_html_response(contents)
 
-                limit = arguments.get("limit", [None])[0]
-                if limit and limit.isdigit():
-                    species = species[:int(limit)]
-                else:
-                    limit = len(species)
+    def listSpecies(self, arguments):
+        data = self.get_ensembl_json("/info/species")
+        if data:
+            species = data["species"]
 
-                species_list = data["species"][:int(limit)]
-                names_list = []
-                for i in species_list:
-                    name = i["display_name"]
-                    names_list.append(name)
-                for name in names_list:
-                    names_list += f"<li>{name}</li>"
+            limit = arguments.get("limit", [None])[0]
+            if limit and limit.isdigit():
+                species = species[:int(limit)]
+            else:
+                limit = len(species)
 
-                contents = Path('listSpecies.html').read_text()
-                result = contents.format(
-                    total_species=len(data["species"]),
-                    limit_value=limit,
-                    species_list=names_list
-                )
-                self.send_response(200)
-                self.send_header('Content-Type', 'text/html')
-                self.send_header('Content-Length', len(result.encode()))
-                self.end_headers()
-                self.wfile.write(result.encode())
+            species_list = data["species"][:int(limit)]
+            names_list = []
+            for i in species_list:
+                name = i["display_name"]
+                names_list.append(name)
+            species_html = self.html_lists(names_list)
 
+            contents = Path('listSpecies.html').read_text()
+            result = contents.format(
+                total_species=len(data["species"]),
+                limit_value=limit,
+                species_list=species_html
+            )
+            self.send_html_response(result)
+        else:
+            self.error()
+            print("Error: Ensembl data could not be obtained")
+
+    def karyotype(self, arguments):
+        specie_selected = arguments.get("species", [None])[0]
+        data = self.get_ensembl_json(f"/info/assembly/{specie_selected}")
+        if data:
+            regions = data["top_level_region"]
+            names_list = []
+            for region in regions:
+                name = region["name"]
+                names_list.append(name)
+            names_html = self.html_lists(names_list)
+
+            contents = Path('karyotype.html').read_text()
+            result = contents.format(
+                names_list=names_html
+            )
+            self.send_html_response(result)
 
         else:
-            contents = Path('error.html').read_text()
+            self.error()
+            print("Error: Ensembl data could not be obtained")
 
-            self.send_response(404)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(contents.encode()))
-            self.end_headers()
-            self.wfile.write(contents.encode())
-        return
+
+    def chromosomeLength(self, arguments):
+        specie_selected = arguments.get("species", [None])[0]
+        chromosome_selected = arguments.get("chromosome", [None])[0]
+        data = self.get_ensembl_json(f"/info/assembly/{specie_selected}")
+
+        if data:
+            regions = data["top_level_region"]
+            length = None
+
+            for region in regions:
+                if str(region["name"]) == str(chromosome_selected):
+                    length = region["length"]
+                break
+
+            if length:
+                contents = Path('chromosomeLength.html').read_text()
+                result = contents.format(
+                    length_chromosome=length
+                )
+                self.send_html_response(result)
+                print(f"{length}")
+            else:
+                self.error()
+        else:
+            self.error()
+            print("Error: Ensembl data could not be obtained")
+
+
+
+
+
+
+
+
 
 Handler = TestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
